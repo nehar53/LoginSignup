860a295eb6afcfd9759f275486c4309d425874ef
nehar53 <nehar5383@users.noreply.github.com>
2020-07-21 22:45:22 +0530

    first commit

---
+++ app/src/main/java/com/nehar5383/loginsignup/MainActivity.java
@@ -0,0 +1,58 @@
+package com.nehar5383.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import com.example.loginapp.R;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragmet(new LoginFragment());
+        pagerAdapter.addFragment(new RegisterFragment());
+      //  pagerAdapter.addFragment(new Activity2());
+
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public AuthenticationPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragmet(LoginFragment fragment) {
+            fragmentList.add(fragment);
+
+        }
+        void addFragment(RegisterFragment fragment) {
+            fragmentList.add(fragment);
+        }
+
+
+    }
+}
